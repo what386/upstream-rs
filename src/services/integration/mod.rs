6f1cb13809e6cdc03560674440b99c86d9c449a7
bmorin <robot0324@gmail.com>
2026-02-12 15:32:04 -0500

    rename all services to *_manager

--- src/services/integration/mod.rs
+++ src/services/integration/mod.rs
@@ -2,13 +2,13 @@ pub mod compression_handler;
 pub mod permission_handler;
 
 mod appimage_extractor;
-mod desktop_handler;
-mod icon_handler;
-mod shell_handler;
-mod symlink_handler;
+mod desktop_manager;
+mod icon_manager;
+mod shell_manager;
+mod symlink_manager;
 
 pub use appimage_extractor::AppImageExtractor;
-pub use desktop_handler::DesktopManager;
-pub use icon_handler::IconManager;
-pub use shell_handler::ShellManager;
-pub use symlink_handler::SymlinkManager;
+pub use desktop_manager::DesktopManager;
+pub use icon_manager::IconManager;
+pub use shell_manager::ShellManager;
+pub use symlink_manager::SymlinkManager;
