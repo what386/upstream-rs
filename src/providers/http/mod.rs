24f01ec181eea0a7d85d7b30f73769e81a548950
bmorin <robot0324@gmail.com>
2026-02-13 20:07:08 -0500

    cargo format

--- src/providers/http/mod.rs
+++ src/providers/http/mod.rs
@@ -1,7 +1,7 @@
+pub mod direct_adapter;
 pub mod http_client;
 pub mod webscraper_adapter;
-pub mod direct_adapter;
 
+pub use direct_adapter::DirectAdapter;
 pub use http_client::HttpClient;
 pub use webscraper_adapter::WebScraperAdapter;
-pub use direct_adapter::DirectAdapter;
