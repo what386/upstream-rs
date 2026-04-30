f4b9d25f82c4c247caca9d6edec23dd1137bf27c
bmorin <robot0324@gmail.com>
2026-04-29 20:37:15 -0400

    rename

--- src/services/trust/mod.rs
+++ src/services/trust/mod.rs
@@ -1,7 +1,7 @@
 pub mod checksum_verifier;
 pub mod signature_verifier;
-pub mod trust_verifier;
+pub mod trust_pipeline;
 
 pub use checksum_verifier::ChecksumVerifier;
 pub use signature_verifier::{MinisignPublicKey, SignatureVerificationStatus, SignatureVerifier};
-pub use trust_verifier::{ChecksumVerificationStatus, TrustVerificationStatus, TrustVerifier};
+pub use trust_pipeline::{ChecksumVerificationStatus, TrustVerificationStatus, TrustVerifier};
