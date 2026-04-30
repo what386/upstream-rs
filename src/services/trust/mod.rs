14f46c49382bf80d464092731c6509d91e570002
bmorin <robot0324@gmail.com>
2026-04-29 20:03:07 -0400

    add trust verifier

--- src/services/trust/mod.rs
+++ src/services/trust/mod.rs
@@ -1,5 +1,7 @@
 pub mod checksum_verifier;
 pub mod signature_verifier;
+pub mod trust_verifier;
 
 pub use checksum_verifier::ChecksumVerifier;
 pub use signature_verifier::{MinisignPublicKey, SignatureVerificationStatus, SignatureVerifier};
+pub use trust_verifier::{ChecksumVerificationStatus, TrustVerificationStatus, TrustVerifier};
