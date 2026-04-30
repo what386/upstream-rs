71e695fc84da65e92394c981502118b95f2c40cc
bmorin <robot0324@gmail.com>
2026-04-29 20:28:35 -0400

    upd interface

--- src/models/upstream/mod.rs
+++ src/models/upstream/mod.rs
@@ -2,6 +2,6 @@ pub mod app_config;
 pub mod package;
 pub mod package_reference;
 
-pub use app_config::AppConfig;
+pub use app_config::{AppConfig, MinisignKeyConfig};
 pub use package::{InstallType, Package};
 pub use package_reference::PackageReference;
