82f2dc9ed690619c4dba28e6aa32551a9ed22085
bmorin <robot0324@gmail.com>
2026-04-29 20:03:42 -0400

    add trust to config

--- src/models/upstream/app_config.rs
+++ src/models/upstream/app_config.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use crate::services::trust::MinisignPublicKey;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(default)]
@@ -18,8 +19,35 @@ impl Default for ProviderConfig {
 
 #[derive(Debug, Clone, Serialize, Deserialize, Default)]
 #[serde(default)]
+pub struct MinisignKeyConfig {
+    pub id: Option<String>,
+    pub key: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, Default)]
+#[serde(default)]
+pub struct TrustConfig {
+    pub minisign_public_keys: Vec<MinisignKeyConfig>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, Default)]
+#[serde(default)]
 pub struct AppConfig {
     pub github: ProviderConfig,
     pub gitlab: ProviderConfig,
     pub gitea: ProviderConfig,
+    pub trust: TrustConfig,
+}
+
+impl AppConfig {
+    pub fn trusted_minisign_keys(&self) -> Vec<MinisignPublicKey> {
+        self.trust
+            .minisign_public_keys
+            .iter()
+            .map(|k| MinisignPublicKey {
+                id: k.id.clone(),
+                key: k.key.clone(),
+            })
+            .collect()
+    }
 }
