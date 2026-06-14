7901f13d9efbad494e1ebb9744ca3e49528fe607
what386 <robot0324@gmail.com>
2026-06-14 01:15:03 -0400

    rename

--- src/models/provider/mod.rs
+++ src/models/provider/mod.rs
@@ -1,9 +1,9 @@
 pub mod asset;
 pub mod release;
-pub mod repository_search_filters;
-pub mod repository_search_result;
+pub mod search_filters;
+pub mod search_result;
 
 pub use self::asset::Asset;
 pub use self::release::Release;
-pub use self::repository_search_filters::RepositorySearchFilters;
-pub use self::repository_search_result::RepositorySearchResult;
+pub use self::search_filters::RepositorySearchFilters;
+pub use self::search_result::RepositorySearchResult;
