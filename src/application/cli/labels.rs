2239f87e1b7d0be841eee966cbc47ea27dacd283
what386 <robot0324@gmail.com>
2026-06-14 14:02:32 -0400

    add rollback subcommands, among other things

--- src/application/cli/labels.rs
+++ src/application/cli/labels.rs
@@ -1,6 +1,8 @@
 use std::fmt;
 
-use crate::application::cli::arguments::{Commands, ConfigAction, HooksAction, PackageAction};
+use crate::application::cli::arguments::{
+    Commands, ConfigAction, HooksAction, PackageAction, RollbackAction,
+};
 
 impl fmt::Display for Commands {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -8,7 +10,7 @@ impl fmt::Display for Commands {
             Commands::Install { .. } => write!(f, "install"),
             Commands::Build { .. } => write!(f, "build"),
             Commands::Remove { .. } => write!(f, "remove"),
-            Commands::Rollback { .. } => write!(f, "rollback"),
+            Commands::Rollback { action } => write!(f, "{action}"),
             Commands::Reinstall { .. } => write!(f, "reinstall"),
             Commands::Upgrade { .. } => write!(f, "upgrade"),
             Commands::List { .. } => write!(f, "list"),
@@ -56,6 +58,16 @@ impl fmt::Display for PackageAction {
             PackageAction::Pin { .. } => write!(f, "package pin"),
             PackageAction::Unpin { .. } => write!(f, "package unpin"),
             PackageAction::Rename { .. } => write!(f, "package rename"),
+        }
+    }
+}
+
+impl fmt::Display for RollbackAction {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            RollbackAction::Restore { .. } => write!(f, "rollback restore"),
+            RollbackAction::Prune { .. } => write!(f, "rollback prune"),
+            RollbackAction::List => write!(f, "rollback list"),
         }
     }
 }
