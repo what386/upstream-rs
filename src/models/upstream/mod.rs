fd72265b9378713cb6ff420bd75dda2c2811645d
what386 <robot0324@gmail.com>
2026-07-17 12:58:01 -0400

    rn

--- src/models/upstream/mod.rs
+++ src/models/upstream/mod.rs
@@ -1,8 +1,8 @@
 pub mod config;
 pub mod authentication;
 pub mod package;
-pub mod package_reference;
+pub mod package_ref;
 
 pub use authentication::{AuthenticationConfig, ProviderAuthentication};
 pub use package::{InstallType, Package};
-pub use package_reference::PackageReference;
+pub use package_ref::PackageReference;
